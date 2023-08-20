17a23fedbf7dc87730dd14aaaaa098e1bba6aebe
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-08-20 23:11:11 +0300

    complete lectures 1-11

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,69 @@
+/*
 let js = "amazing";
-if (js === "amazing") {
-    alert("JavaScript is FUN!");
-}
 
-console.log(40 + 8 + 23 - 10);
+// if (js === "amazing") {
+//     alert("JavaScript is FUN!");
+// }
+
+// console.log(40 + 8 + 23 - 10);
+
+let firstName = "Andrii";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name convention
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+
+const PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// bad practice
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "javascriptIsFun");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// bug - show object
+console.log(typeof null);
+
+
+
+// Assignments
+let country = "Ukraine";
+let continent = "Europe";
+let population = 30000000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
