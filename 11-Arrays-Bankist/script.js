daecf7ba8a5de15fd43f8ab5da1268ac7b8080ac
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-09 16:56:13 +0300

    Lesson 153 The reduce Method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -261,6 +261,7 @@ console.log(movementsDescriptions);
 
 ///////////////////////////////////////
 // The filter Method
+/*
 const deposits = movements.filter(function (mov, i, arr) {
   return mov > 0;
 });
@@ -273,3 +274,28 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+
+ */
+
+///////////////////////////////////////
+// The reduce Method
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
