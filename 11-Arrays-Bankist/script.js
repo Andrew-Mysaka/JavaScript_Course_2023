65ed3b8493592b2aafa06abdf1efee974a3ecf8f
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-09 18:14:20 +0300

    Lesson 156 Challenge #3

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -345,6 +345,7 @@ console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 
 ///////////////////////////////////////
 // The Magic of Chaining Methods
+/*
 const eurToUsd = 1.1;
 console.log(movements);
 
@@ -358,3 +359,27 @@ const totalDepositsUSD = movements
   // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+ */
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this
+time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(dogAge => dogAge <= 2 ? dogAge * 2 : 16 + dogAge * 4)
+    .filter(age => age > 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
