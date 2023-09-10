7e732c24a55e0dde4b0cc049102cb8b43426a021
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-10 15:42:35 +0300

    Lesson 162 flat and flatMap

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -402,7 +402,7 @@ console.log(account);
 
 ///////////////////////////////////////
 // some and every
-
+/*
 console.log(movements);
 
 // EQUALITY
@@ -423,5 +423,34 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
 
+///////////////////////////////////////
+// flat and flatMap
 
+const arr = [[1,2,3], [4,5,6],7,8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overallBalance = allMovements
+//   .reduce((acc,mov) => acc + mov, 0);
+// console.log(overallBalance);
+
+// flat
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// flatMap
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
