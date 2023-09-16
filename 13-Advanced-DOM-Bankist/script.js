36f4f0573f091865f0b77732240156f45da96dde
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-16 17:08:38 +0300

    Lesson 196 Sticky navigation: Intersection Observer API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -211,8 +211,25 @@ const initialCoords = section1.getBoundingClientRect();
 console.log(initialCoords);
 
 window.addEventListener('scroll', function () {
-  console.log(window.scrollY);
+  //console.log(window.scrollY);
 
   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 });
+
+///////////////////////////////////////
+// Sticky navigation: Intersection Observer API
+
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
