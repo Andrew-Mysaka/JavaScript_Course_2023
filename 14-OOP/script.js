4f6903289281d768b7c9dd5820062ab4db5de563
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-17 15:05:29 +0300

    Lesson 213 ES6 Classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -126,3 +126,42 @@ bmw.brake();
 bmw.accelerate();
 bmw.brake();
 bmw.accelerate();
+
+///////////////////////////////////////
+// ES6 Classes
+
+// Class expression
+// const PersonCl = class {}
+
+// Class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.fullName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
