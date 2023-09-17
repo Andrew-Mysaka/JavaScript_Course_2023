997da489d86718e8fe2f8f9d4cc4e74fe3d4144c
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-17 10:07:44 +0300

    Lesson 208 Constructor Functions and the new Operator

---
+++ 14-OOP/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+///////////////////////////////////////
+// Constructor Functions and the new Operator
+
+const Person = function(firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never to this!
+  // this.calcAge = function() {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const andrii = new Person('Andrii', 1985);
+console.log(andrii);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(andrii instanceof Person);
+
