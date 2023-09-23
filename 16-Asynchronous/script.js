e7d9d05f4c9e0ef6c7a2531ca67e9e00baf69daf
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-23 11:44:03 +0300

    Lesson 253 Chaining Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -117,7 +117,7 @@ setTimeout(() => {
 */
 /////////////////////////////////////////
 // Consuming Promises
-
+// Chaining Promises
 
 // const getCountryData = function (country) {
 //   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
@@ -132,9 +132,18 @@ setTimeout(() => {
 // };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
     .then((response) => response.json())
-    .then((data) => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      // Country 2
+      return fetch(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('portugal');
