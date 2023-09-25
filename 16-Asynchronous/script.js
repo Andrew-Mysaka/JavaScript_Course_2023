6f34ce0ef31c86ac4a2bf83094d927c9fccc2af5
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-25 21:38:51 +0300

    Lesson 263 Error Handling With try...catch

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -447,6 +447,7 @@ createImage('img/img-1.jpg')
 
 ///////////////////////////////////////
 // Consuming Promises with Async/Await
+// Error Handling With try...catch
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -456,23 +457,36 @@ const getPosition = function () {
 
 // fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => console.log(res))
 
-const whereAmI = async function() {
-  // Geolocation
-  const pos = await getPosition();
-  const { latitude: lat, longitude: lng } = pos.coords;
+const whereAmI = async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
 
-  // Reverse geocoding
-  const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    // Reverse geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
 
-  const dataGeo = await resGeo.json();
-  console.log(dataGeo);
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
 
-  // Country data
-  const res = await fetch(`https://countries-api-836d.onrender.com/countries/name/${dataGeo.country}`);
-  const data = await res.json();
-  console.log(data);
-  renderCountry(data[0]);
+    // Country data
+    const res = await fetch(
+      `https://countries-api-836d.onrender.com/countries/name/${dataGeo.country}`
+    );
+
+    if (!res.ok) throw new Error('Problem getting country');
+
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+  }
 };
 
 whereAmI();
+// whereAmI();
+// whereAmI();
 console.log('FIRST');
