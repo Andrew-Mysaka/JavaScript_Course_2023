15a3cfca1615c078fa489ad7318bb8397df0d13f
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-09-30 18:32:24 +0300

    Lesson 279 Configuring Babel and Polifilling

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -119,3 +119,29 @@ state.user.loggedIn = false;
 console.log(stateClone);
 
 console.log(stateDeepClone);
+
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const andrii = new Person('Andrii');
+
+console.log('Andrii' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
